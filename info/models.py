d4e4b8a889f3fe003fa71f64eac4e9c7affa7acc
Adegite Taiwo <adegitetaiwo24@gmail.com>
2020-11-27 00:31:49 +0100

    changed 'updated' in infoModel to DateTimeField

--- info/models.py
+++ info/models.py
@@ -7,7 +7,7 @@ class infoModel(models.Model):
     topic = models.CharField(max_length=80)
     info = models.TextField()
     created = models.DateField(auto_now=False, auto_now_add=True)
-    updated = models.DateField(auto_now=True, auto_now_add=False)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __str__(self):
         return self.topic
