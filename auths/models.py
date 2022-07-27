c2cee303848cdfc6ce06507cdac606397c9b8383
Animashaun olasunkanmi <animashaunstephen@gmail.com>
2022-07-27 14:15:29 +0100

    Update models.py
    Made a change to models.py

--- auths/models.py
+++ auths/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    course = models.CharField(max_length=290)
-    password = models.CharField(max_length=80, editable=False)
-    confirm_password = models.CharField(max_length=50, editable=False)
+    user_id = models.OneToOneField
+    username = models.CharField(max_length=50)
+    profile_pic = models.ImageField()
+    profile_bio = models.TextField()
+    
 
     def __str__(self):
-        return self.username.username
+        return self.username
